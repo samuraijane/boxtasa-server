88ad7cf08d3e0a97bc643ffff45fd250543db5ff
Matthew Day <matt@matthewday.net>
2019-03-20 15:43:30 -0400

    Create server

---
+++ server.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+// https://stackoverflow.com/questions/51960171/node63208-deprecationwarning-collection-ensureindex-is-deprecated-use-creat
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+const app = express();
+app.use(bodyParser.json());
+
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, Origin, X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'DELETE, GET, PATCH, POST, PUT');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
+const {PORT, LOCAL_DB, CLOUD_DB} = require('./config');
+const {codesRouter} = require('./routes');
+
+app.use('/codes', codesRouter);
+
+
+let server;
+
+// for local database, use "databaseUrl=LOCAL_DB"
+// for hosted database, use "databaseUrl=CLOUD_DB"
+function runServer(databaseUrl=LOCAL_DB, port=PORT) {
+  let promise = new Promise( (resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if(err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`The server is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+  return promise;
+}
+
+function closeServer() {
+  return mongoose.disconnect()
+    .then( () => {
+      let promise = new Promise( (resolve, reject) => {
+        console.log('Closing server...');
+        server.close(err => {
+          if(err) {
+            return reject(err);
+          }
+          resolve();
+        })
+      });
+      return promise;
+    });
+}
+
+if(require.main === module) {
+  runServer()
+  .catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
