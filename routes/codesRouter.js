6762392ffbd6d470f7cbc03d9b42b44f96257268
Matthew Day <matt.day@shakerinternational.com>
2019-09-30 06:42:39 -0400

    Add error details to response

--- routes/codesRouter.js
+++ routes/codesRouter.js
@@ -1,71 +1,69 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
-const ObjectId = require('mongodb').ObjectID;
-const {Code} = require('./../models');
-
+const ObjectId = require("mongodb").ObjectID;
+const { Code } = require("./../models");
 
 // -----------------------------------------------------------------------------
 //                                    DELETE
 // -----------------------------------------------------------------------------
 
-
 // -----------------------------------------------------------------------------
 //                                    GET ALL
 // -----------------------------------------------------------------------------
-router.get('/', (req, res) => {
-  Code
-  .find()
-  .exec()
-  .then(codes => {
-    res.json(codes.map( code => code.apiRepr() ));
-  })
-  .catch(err => {
-    console.log(err);
-    return res.status(500).json({message: 'Internal server error'});
-  });
+router.get("/", (req, res) => {
+  Code.find()
+    .exec()
+    .then(codes => {
+      res.json(codes.map(code => code.apiRepr()));
+    })
+    .catch(err => {
+      console.log(err);
+      return res
+        .status(500)
+        .json({ message: "Internal server error", error: err });
+    });
 });
-
 
 // -----------------------------------------------------------------------------
 //                                  GET SINGLE
 // -----------------------------------------------------------------------------
-router.get('/:id', (req, res) => {
-  Code
-  .findById(req.params.id)
-  .exec()
-  .then( code => {
-    res.json({
-      code: code.apiRepr()
+router.get("/:id", (req, res) => {
+  Code.findById(req.params.id)
+    .exec()
+    .then(code => {
+      res.json({
+        code: code.apiRepr()
+      });
     })
-  })
-  .catch(err => {
-    console.log(err);
-    return res.status(500).json({message: 'Internal server error'});
-  });
+    .catch(err => {
+      console.log(err);
+      return res.status(500).json({ message: "Internal server error" });
+    });
 });
-
-
-
 
 // -----------------------------------------------------------------------------
 //                                     POST
 // -----------------------------------------------------------------------------
 
-
-
-
 // -----------------------------------------------------------------------------
 //                                      PUT
 // -----------------------------------------------------------------------------
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // TODO finish handling updates to multiple fields– the challenge here are the arrays
-  const updateableFields = ['description', 'keywords', 'sampleNames', 'subs', 'summary', 'title'];
-  if(req.params.id !== req.body.id) {
+  const updateableFields = [
+    "description",
+    "keywords",
+    "sampleNames",
+    "subs",
+    "summary",
+    "title"
+  ];
+  if (req.params.id !== req.body.id) {
     const message = `The request path (${req.params.id}) and the request body id (${req.body.id}) must match.`;
     console.error(message);
-    return res.status(400).json({message: message});
+    return res.status(400).json({ message: message });
   }
   // const singleFieldsToUpdate = {};
   // const subsFieldToUpdate = [];
@@ -79,22 +77,22 @@ router.put('/:id', (req, res) => {
   //   }
   // });
 
-  Code.updateOne({_id: new ObjectId(req.params.id)},
-  {
-    $set: {
-      subs: req.body.subs
+  Code.updateOne(
+    { _id: new ObjectId(req.params.id) },
+    {
+      $set: {
+        subs: req.body.subs
+      }
     }
-  })
-  .exec()
-  .then(code => {
-    return res.status(204).end();
-  })
-  .catch(err => {
-    console.error(err);
-    return res.status(500).json({message: 'Internal server error'})
-  });
-
+  )
+    .exec()
+    .then(code => {
+      return res.status(204).end();
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ message: "Internal server error" });
+    });
 });
 
-
-module.exports = {router};
+module.exports = { router };
